acf59fdbadd194808a3d2aa1591a53a63c8a73ea
Robin Hasler <robin.hasler@bluewin.ch>
2024-04-20 13:51:53 +0200

    navigation update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,15 +13,15 @@ import {
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 const navigation = [
-  { name: "Home", href: "./home", icon: HomeIcon, current: true },
+  { name: "Home", href: "/home", icon: HomeIcon, current: true },
   {
     name: "Documents",
-    href: "./documents",
+    href: "/documents",
     icon: DocumentDuplicateIcon,
     current: false,
   },
-  { name: "Report", href: "./report", icon: ChartPieIcon, current: false },
-  { name: "Contact", href: "./contact", icon: UsersIcon, current: false },
+  { name: "Report", href: "/report", icon: ChartPieIcon, current: false },
+  { name: "Contact", href: "/contact", icon: UsersIcon, current: false },
 ];
 const teams = [
   { id: 1, name: "Eingang", href: "#", initial: "M", current: false },
